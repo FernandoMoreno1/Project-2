2267061b94d6b9044d025e0aa41b9b63b13cfba9
JCIII <jcostello711@gmail.com>
2022-04-12 16:39:40 -0400

    realized Onwer model is not needed. User model will just have a property to track if the User is an owner

---
+++ controllers/home-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+
+// get all posts for homepage
+router.get('/', (req, res) => {
+    console.log('======================');
+    User.findAll({
+    attributes: [
+        'id',
+        'first_name',
+        'last_name',
+    ],
+    })
+    .then(dbPostData => {
+        const users = dbPostData.map(user => user.get({ plain: true }));
+
+        res.render('homepage', { users });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
