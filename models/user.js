7b4f4d8b173f409cd10644c6ceb8552308ff70d0
Hilary V <92323804+hvasconez24@users.noreply.github.com>
2022-04-13 04:34:07 -0500

    model/user

--- models/user.js
+++ models/user.js
@@ -1,68 +1,91 @@
 const { Model, DataTypes } = require('sequelize');
-//este archivo no existe aun
-const sequelize = require('../connection/database');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-class User extends Model {}
-
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [6]
-      }
-    },
-    address:{
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    number:{
-        type: DataTypes.NUMBER,
-        allowNull: true
-    },
-    role:{
-        type: DataTypes.ENUM('Customer','ROwner'),
-        allowNull: false
-    },
-    profileState:{
-        type: DataTypes.ENUM('incomplete','complete'),
-        defaultValue: "incomplete"
-    },
-    id_image:{
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'image',
-            key: 'id'
-        },
-        allowNull: true
+// User model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
     }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user'
-  }
+}
+
+// create fields/columns for User model
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlpha: true
+            }
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlpha: true
+            }
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isAlphanumeric: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        phone_number: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlphanumeric: true
+            }
+        }
+    },
+    {
+        hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+        },
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        modelName: 'user'
+    }
 );
 
 module.exports = User;
